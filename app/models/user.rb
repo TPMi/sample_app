3693b9f3956921522f9fa44d30a25f285cca0471
TPMi <tpmi@me.com>
2011-02-04 14:45:41 +0300

    Improved user model with new authenticate method

--- app/models/user.rb
+++ app/models/user.rb
@@ -29,9 +29,10 @@ class User < ActiveRecord::Base
   
   def self.authenticate(email, submitted_password)
     user = find_by_email(email)
-    return nil if user.nil?
-    return user if user.has_password?(submitted_password)
-    return nil # else - implicitly done, but I've inserted this for clearance
+    user && user.has_password?(submitted_password) ? user : nil
+    # return nil if user.nil?
+    # return user if user.has_password?(submitted_password)
+    # return nil # else - implicitly done, but I've inserted this for clearance
   end
 
   private
